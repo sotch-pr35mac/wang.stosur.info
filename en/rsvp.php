a42f627b85e5730898a4c67900d402bcc984e8e8
Preston Stosur-Bassett <preston@stosur.info>
2016-10-24 18:40:02 +0800

    added more rsvp stuff

--- en/rsvp.php
+++ en/rsvp.php
@@ -1,14 +1,14 @@
-<?php 
+<?php
 
 	// IF VALIDATION ERROR
 	if (isset($_POST["all_error_required"])){
 		$output = json_encode(array('type'=>'error', 'text' => $_POST["all_error_required"][0]));
 		die($output);
 	}
-	
+
 	// IF NO ERROR
 	if (isset($_POST["all_input_id"])){
-		
+
 		$finalmessage = "";
 		foreach ($_POST["all_input_id"] as $input_id) {
 			if (is_array($_POST[$input_id])){
@@ -19,18 +19,18 @@
 				$finalmessage .= $_POST[$input_id."_label"]." : ". $_POST[$input_id] . "\n\n";
 			}
 		}
-	
-		$email_to  =  'your@email.com'; 
-		
-		$headers = "From: ".$_POST["inputemail"]."\r\n";	
-		$headers .= "Reply-To: ".$_POST["inputemail"]."\r\n";	
-		$subject = "RSVP message from Mr/Mrs ".$_POST["inputname"];	
-				
+
+		$email_to  =  'pstosurbassett@icloud.com'; 
+
+		$headers = "From: ".$_POST["inputemail"]."\r\n";
+		$headers .= "Reply-To: ".$_POST["inputemail"]."\r\n";
+		$subject = "RSVP message from Mr/Mrs ".$_POST["inputname"];
+
 		if(mail($email_to, $subject, $finalmessage, $headers)){
         	$output = json_encode(array('type'=>'success', 'text' => 'Message Sent'));
     	}else{
         	$output = json_encode(array('type'=>'error', 'text' => 'Failed'));
-   		}		
-		die($output);	
-	}	
+   		}
+		die($output);
+	}
 ?>
